2f3e85cf81c9439c5441784b4c6cc239d671bb3e
Dooyong Kim <kdooyong@amazon.com>
2026-01-16 19:43:31 -0800

    Fix MOS reentrant search bug in byte index.
    Signed-off-by: Dooyong Kim <kdooyong@amazon.com>

--- src/main/java/org/opensearch/knn/index/query/nativelib/NativeEngineKnnVectorQuery.java
+++ src/main/java/org/opensearch/knn/index/query/nativelib/NativeEngineKnnVectorQuery.java
@@ -24,6 +24,7 @@ import org.apache.lucene.search.knn.TopKnnCollectorManager;
 import org.apache.lucene.util.Bits;
 import org.opensearch.common.StopWatch;
 import org.opensearch.knn.index.KNNSettings;
+import org.opensearch.knn.index.VectorDataType;
 import org.opensearch.knn.index.query.ExactSearcher;
 import org.opensearch.knn.index.query.KNNQuery;
 import org.opensearch.knn.index.query.KNNWeight;
@@ -368,7 +369,7 @@ public class NativeEngineKnnVectorQuery extends Query {
             final ReentrantKnnCollectorManager reentrantCollectorManager = new ReentrantKnnCollectorManager(
                 new TopKnnCollectorManager(k, indexSearcher),
                 segmentOrdToResults,
-                knnQuery.getQueryVector(),
+                knnQuery.getVectorDataType() == VectorDataType.FLOAT ? knnQuery.getQueryVector() : knnQuery.getByteQueryVector(),
                 knnQuery.getField()
             );
 
